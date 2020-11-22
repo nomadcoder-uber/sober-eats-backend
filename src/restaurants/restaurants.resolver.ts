87a7992cc7df028d373d79b1d4688cea40450ca2
somin <qkrthals0524@naver.com>
2020-11-22 22:30:50 +0900

    #1.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,13 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { Restaurant } from "./entities/restaurant.entitiy";
 
 
 
 @Resolver(of => Restaurant) // resolver는 Restaurant의 resolver가 된다
 export class RestaurantResolver{
-    @Query(returns => Restaurant) 
-    myRestaurant(){
-        return true;
+    @Query(returns => [Restaurant]) 
+    restaurants(@Args('veganOnly') veganOnly: Boolean): Restaurant[]{
+        return [];
     }
    
 }
