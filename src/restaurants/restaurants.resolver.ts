4c9f99d71e3bb6519b23708026168a33cbf16565
somin <qkrthals0524@naver.com>
2020-11-22 21:58:14 +0900

    #1.2 ObjectType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,15 @@
-import { Query,Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from "@nestjs/graphql";
+import { Restaurant } from "./entities/restaurant.entitiy";
 
 
 
-@Resolver()
+@Resolver(of => Restaurant) // resolver는 Restaurant의 resolver가 된다
 export class RestaurantResolver{
-    @Query(() =>Boolean)
-    isPizzaGood():Boolean {
+    @Query(returns => Restaurant) 
+    myRestaurant(){
         return true;
     }
-
-
+   
 }
+
+
