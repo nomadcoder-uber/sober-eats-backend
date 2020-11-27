cd6a82d3ba1fdf42210743eb34c2ff1832635509
somin <qkrthals0524@naver.com>
2020-11-27 12:53:50 +0900

    Injecting The Repository and Use Data Mapper

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,14 +1,16 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entitiy";
+import { RestaurantService } from "./restaurants.service";
 
 
 
 @Resolver(of => Restaurant) // resolver는 Restaurant의 resolver가 된다
 export class RestaurantResolver{
+    constructor(private readonly restaurantService: RestaurantService){}
     @Query(returns => [Restaurant]) 
-    restaurants(@Args('veganOnly') veganOnly: Boolean): Restaurant[]{ // Restaurant라는 빈 리스트를 리턴
-        return [];
+    restaurants():Promise<Restaurant[]>{ // Restaurant라는 빈 리스트를 리턴
+        return this.restaurantService.getAll();
     }
     @Mutation(returns => Boolean)
     createRestaurant(
