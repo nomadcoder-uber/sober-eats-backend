ce6c640ce219054d0159fb197b0d83a34cf270dd
somin <qkrthals0524@naver.com>
2020-11-28 11:54:39 +0900

    #3.4 Optional Types and Columns

--- src/restaurants/entities/restaurant.entitiy.ts
+++ src/restaurants/entities/restaurant.entitiy.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
@@ -13,22 +14,19 @@ export class Restaurant {
 
     @Field(type => String)
     @Column()
+    @IsString()
+    @Length(5)
     name: string;
 
-    @Field(type => Boolean)
-    @Column()
-    isVegan?:boolean;
+    @Field(type => Boolean, {nullable:true}) //graphql 스키마 설정
+    @Column({default:true}) //database에 반영
+    @IsOptional() //해당 필드를 보내거나 보내지 않을수있다 (validation 설정)
+    @IsBoolean()
+    isVegan:boolean;
 
-    @Field(type=> String)
+    @Field(type=> String, {defaultValue:"강남"})
     @Column()
+    @IsString()
     address:string;
-
-    @Field(type=>String)
-    @Column()
-    ownerName:string;
-
-    @Field(type=>String)
-    @Column()
-    categoryName: string;
 
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -18,6 +18,7 @@ export class RestaurantResolver{
     async createRestaurant(
         @Args('input') CreateRestaurantDto: CreateRestaurantDto,
     ): Promise<boolean> {
+        console.log(CreateRestaurantDto)
         try{
             await this.restaurantService.createRestaurant(CreateRestaurantDto);
             return true;
