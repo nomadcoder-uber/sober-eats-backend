987bcd63b4515a1df7510d00bb5495fd2f331562
somin <qkrthals0524@naver.com>
2020-12-14 21:59:45 +0900

    #5.11 Recap

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -12,13 +12,14 @@ export class JwtMiddleware implements NestMiddleware { //상속받음
   async use(req: Request, res: Response, next: NextFunction) {
     if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
-      const decoded = this.jwtService.verify(token.toString());
-      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) { //여기서 가져온 id를 가지고 user를 찾아야 한다
-        try {
+      try {
+        const decoded = this.jwtService.verify(token.toString());
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) { //여기서 가져온 id를 가지고 user를 찾아야 한다
+        
           const user = await this.userService.findById(decoded['id']);
           req['user'] = user;
-        } catch (e) {}
-      }
+        } 
+      }catch (e) {}
     }
     next();
   }
