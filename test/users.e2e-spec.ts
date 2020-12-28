1d1eab2c04d75b8b985a6af8675f6aa57f5181bd
somin <qkrthals0524@naver.com>
2020-12-28 15:37:58 +0900

    9.3 Testing createAccount part Two

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -5,6 +5,12 @@ import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
 
+jest.mock('got',()=> {
+  return {
+    post:jest.fn(),
+  }
+})
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
@@ -50,8 +56,34 @@ describe('UserModule (e2e)', () => {
       })
 
     })
-    it.todo('userProfile')
+
+    it('should fail if account already exists',()=>{
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query:`
+        mutation{
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false);
+        expect(res.body.data.createAccount.error).toBe('There is a user with that email already');
+      })
+
+
+    })
   })
+  it.todo('userProfile')
   it.todo('login')
   it.todo('me')
   it.todo('verifyEmail')
