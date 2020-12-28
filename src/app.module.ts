e225b6e8dbc15d76fb3eb15a436bace091859f09
somin <qkrthals0524@naver.com>
2020-12-28 13:50:56 +0900

    9.0 Setup part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { join } from 'path';
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",//우리 폴더에서 .env 파일을 읽는다
       ignoreEnvFile: process.env.NODE_ENV ==="prod",//서버에 deploy 할때 환경변수 파일을 사용하지 않는 옵션
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev','prod').required(),
+        NODE_ENV: Joi.string().valid('dev','prod','test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT : Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
